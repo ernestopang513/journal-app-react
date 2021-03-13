8a9721c86a36b764bbac8e4bf2f061b4b47c6a7f
Ernesto Pang <ernestopang513@hotmail.com>
2021-03-13 00:35:47 -0600

    Empezando a configurar Firestore

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,13 +1,34 @@
 import React from 'react'
+import {useDispatch} from 'react-redux'
 import { Link } from 'react-router-dom'
+import { login } from '../../actions/auth'
+import { useForm } from '../../hooks/useForm'
 
 export const LoginScreen = () => {
+
+    const dispatch = useDispatch()
+
+    const [formValues, handleInputChange] = useForm({
+        email: 'nando@gmail.com',
+        password: '123456'
+    })
+
+    
+    const {email, password} = formValues;
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        dispatch(login(12345, 'Ernesto'));
+
+    }
+
+
     return (
         <>
 
             <h3 className = 'auth__title'>Login</h3>
 
-            <form>
+            <form  onSubmit = {handleLogin}  >
 
                 <input
                     type = 'text'
@@ -15,6 +36,8 @@ export const LoginScreen = () => {
                     name = 'email'
                     className = 'auth__input'
                     autoComplete = 'off'
+                    value = {email}
+                    onChange = {handleInputChange}
                 />
                 
                 <input
@@ -22,6 +45,8 @@ export const LoginScreen = () => {
                     placeholder = 'Password'
                     name = 'password'
                     className = 'auth__input'
+                    value = {password}
+                    onChange = {handleInputChange}
                 />
 
                 <button
